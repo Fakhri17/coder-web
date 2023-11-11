59a7c3647561d6c6fe13262f303849d27212baca
Fakhri17 <fakhricoffe@gmail.com>
2023-11-11 10:04:12 +0700

    fix config

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -15,9 +15,9 @@
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/logo-2.svg";
+      logo.src = "assets/images/coder-logo-warna.png";
     } else {
-      logo.src = "assets/images/logo/logo.svg";
+      logo.src = "assets/images/coder-logo-putih.png";
     }
 
     scrollFunction();
